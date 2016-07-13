4190af34ba549fd21035c27bcd2b311a062c5c8d
zhangyuhao <zhangyuhao1158@sina.com>
2016-07-13 11:11:04 +0800

    Initial Commit

---
+++ block的使用/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  block的使用
+//
+//  Created by zyh on 16/7/13.
+//  Copyright © 2016年 zyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
